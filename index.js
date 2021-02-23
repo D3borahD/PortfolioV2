6ed1c97b33f95e2103dfc5c12bffe620fb4b91e3
Deb <deborah.douillet@yahoo.fr>
2021-02-23 16:56:53 +0100

    Mise en place du scroll sur le boutton Top

---
+++ index.js
@@ -0,0 +1,18 @@
+const navbar = document.querySelector('.navbar');
+const returnTop = document.getElementById('returnTop');
+
+//Apparition de la NavBar format Mobile
+navbar.addEventListener('click', ()=> {
+    navbar.classList.toggle('visible');
+})
+
+//boutton de retour en haut de page
+window.addEventListener('scroll', function(){
+    const scroll = this.document.documentElement.scrollTop;
+
+    if (scroll < 25){
+        returnTop.style.opacity = '0';
+    }else {
+        returnTop.style.opacity = '0.7';
+    }
+})
