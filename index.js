e19e304b9766987e09791fd54de5798f6aad3569
Deb <deborah.douillet@yahoo.fr>
2021-03-05 15:28:24 +0100

    ajustement bloc opencalssrooms

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const lightMode = document.getElementById('light-mode');
 const navbar = document.querySelector('.navbar');
 const returnTop = document.getElementById('returnTop');
 const openClassrooms = document.querySelector('.openclassrooms');
+const listCours = document.querySelector('.liste-cours');
 
 
-// Action à paramétrer 
+
 lightMode.addEventListener('change', () => {
     document.body.classList.toggle('light');
 })
-
 
 //Apparition de la NavBar format Mobile
 navbar.addEventListener('click', ()=> {
